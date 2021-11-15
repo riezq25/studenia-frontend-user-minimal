95ce0869786bdd91788ee0e45612a50297c9b635
Rizqi Ari Putra <riezq.25@gmail.com>
2021-11-15 19:19:44 +0700

    add api

--- src/repositories/Clients/AxiosClient.js
+++ src/repositories/Clients/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const baseDomain = "http://api.studenia.test";
-const baseDomain = 'http://admin.studenia.6digitkode.my.id/'
+const baseDomain = 'http://api.studenia.6digitkode.my.id/'
 const baseURL = `${baseDomain}` // Incase of /api/v1;
 
 const token = localStorage.getItem('token')
